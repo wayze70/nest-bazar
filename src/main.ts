14a8a0c266abc099738cedf6f979e806c40aca88
Michal Syřiště <michalsyriste@seznam.cz>
2023-06-17 21:33:51 +0200

    Update listening port environment

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 
+const port = process.env.PORT || 3030;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.use(cookieParser());
   app.setGlobalPrefix('api/v1');
-  await app.listen(3030);
+  await app.listen(port);
 }
 bootstrap();
